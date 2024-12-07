a2575361ad72646ff6267bfc69ef48215d10877a
ruslana <ruslana.lohvyn@gmail.com>
2024-12-07 21:15:45 +0200

    transactions css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,21 +1,23 @@
+import clsx from "clsx";
 import styles from "./transactionHistory.module.css"
 const TransactionHistory = ({ items }) => {
     
-    return <table className="table">
-        <thead className="table-head">
-            <tr>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Currency</th>
+    return <table className={styles.table}>
+        <thead className={styles.tableHead}>
+            <tr className={styles.tableHeadRow}>
+                <th className={styles.tableHeadData}>Type</th>
+                <th className={styles.tableHeadData}>Amount</th>
+                <th className={styles.tableHeadData}>Currency</th>
             </tr>
         </thead>
 
-        <tbody>
+        <tbody className={styles.tableBody}>
               
-            {items.map((item) =>(<tr key={item.id}>
-                <td>{item.type}</td>
-                <td>{item.amount}</td>
-                <td>{item.currency}</td>
+            {items.map((item) => (<tr key={item.id} className={styles.tableBodyRow}>
+                {/* <td className={styles.tableBodyData}>{item.type}</td> */}
+                <td className={clsx(styles.tableBodyData, styles.type)}>{item.type}</td>
+                <td className={styles.tableBodyData}>{item.amount}</td>
+                <td className={styles.tableBodyData}>{item.currency}</td>
             </tr>))
             }
 
